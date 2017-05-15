b50087e1fc77fa2aa5a7e34629c6cc5b49f30e16
ngouy <nathangouy@free.fr>
2017-05-15 18:31:55 +0200

    on going

--- commands/games/morpion.js
+++ commands/games/morpion.js
@@ -1,12 +1,73 @@
 const commando = require('discord.js-commando');
 const _ = require('underscore');
-const game_cmds = ["test:yolo"];
+const game_cmds = ["A:0", "A:1", "A:2",
+                   "B:0", "B:1", "B:2",
+                   "C:0", "C:1", "C:2"];
+
+const on_going_games = [];
+
+class Morpion {
+
+  /*
+  ** static functions
+  */
+
+  static find(id_or_di) {
+    return _.findWhere(on_going_games, {id: id_or_di}) || _.findWhere(on_going_games, {di: id_or_di}) || null;
+  }
+
+  static if_you_want_to_play(channel, author) {
+    channel.send('if you want to play blablabla');
+  }
+
+  /*
+  ** constructor
+  */
+
+  constructor(player1, player2, channel) {
+    this.grid = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ];
+    this.id = player1.id;
+    this.di = player2.id;
+    this.player1 = player1;
+    this.player2 = player2;
+    this.channel = channel;
+    on_going_games += this;
+    this.lets_play();
+  }
+
+  /*
+  ** Core gaming functions
+  */
+
+  lets_play() {
+
+  }
+
+  process() {
+
+  }
+
+  play(player, move) {
+    channel.send(`#{player.to_s} played ${move}`);
+    this.process();
+  }
+
+}
 
 class MorpionCommand extends commando.Command {
   constructor(client) {
     client.on('message', message => {
       if (_.contains(game_cmds, message.content)) {
-        message.reply('pong');
+        const current_morpion = Morpion.find(message.author.id);
+        if(Morpion.find(message.author.id)) {
+          current_morpion.play(message.author, message.content);
+        } else {
+          Morpion.if_you_want_to_play(message.channel, message.author);
+        }
       }
     });
     // ["test_yolo"].forEach(message => {
