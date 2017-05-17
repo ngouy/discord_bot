c7e134d5e8ef924289d772703cd8ccb43905cac0
ngouy <nathangouy@free.fr>
2017-05-17 13:12:48 +0200

    debug mode initiated

--- commands/games/morpion.js
+++ commands/games/morpion.js
@@ -260,6 +260,10 @@ class MorpionCommand extends commando.Command {
       } else {
         m.turn();
       }
+    } else if (message.content.replace(/ /g,'').toLowerCase() === "!morpiondebug") {
+      on_going_games.forEach(m => {
+        m.turn();
+      });
     } else {
       const mentions = _.uniq(message.mentions.members.array());
       if (m) {
