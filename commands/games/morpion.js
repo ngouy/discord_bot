52f713983e766ea64693fbb018de182976f136bb
ngouy <nathangouy@free.fr>
2017-05-17 21:25:01 +0200

    patch

--- commands/games/morpion.js
+++ commands/games/morpion.js
@@ -5,6 +5,7 @@
 
 const commando = require('discord.js-commando');
 const _ = require('underscore');
+const fs = require('fs');
 const game_cmds = ["A:0", "A:1", "A:2",
                    "B:0", "B:1", "B:2",
                    "C:0", "C:1", "C:2"];
